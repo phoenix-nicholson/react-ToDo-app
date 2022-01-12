dd5a2fb61744bae4855dc49129dad6f2c8d6c5cf
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-01-11 18:06:10 -0800

    fixing netlify build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { NavLink, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { getUser, logout } from './services/users';
 import React, { useState } from 'react';
